9931b3caeeae349e3ab1edfc25512782a91b935a
coolingyou <coolingyou@users.noreply.github.com>
2026-06-03 07:43:28 +0000

    fix: simplify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: '.',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-      external: ['yahoo-finance2'],
-    },
   },
-  server: {
-    p
+})
